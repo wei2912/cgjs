f9efa3f0a39195b49d60946d4cc88ada9b6a657c
Benjamin Tan <demoneauxt@gmail.com>
2015-07-08 18:47:43 +0800

    Updates to L4.

---
+++ dom/template.js
@@ -0,0 +1,63 @@
+/**
+ * This file creates a `ui` object which is used
+ * by `TicTacToe` to update the user interface.
+ */
+
+(function() {
+
+var ui = {};
+
+// Default player.
+ui.defaultPlayer = 'X';
+
+// Starts the game.
+ui.start = function() {
+
+};
+
+// Initializes the elements used.
+ui.initElems = function() {
+
+};
+
+// Creates the Tic-Tac-Toe grid.
+ui.createGrid = function() {
+
+};
+
+// Adds click listeners to each cell
+// and calls `callback` when any cell is clicked.
+ui.addListener = function(callback) {
+
+};
+
+// Sets the given text to the status and makes it visible.
+ui.setStatus = function(text) {
+
+};
+
+// Creates a message informing the user that the game is over.
+// Allows the user to restart by clicking on the status element.
+ui.gameOver = function(type, player) {
+
+};
+
+// Called when a cell is clicked on.
+ui.onCellSelected = function(i) {
+
+};
+
+// Renders the current moves on the board.
+ui.render = function() {
+
+};
+
+// A utility function used to select elements
+// based on their CSS selector.
+function $(selector) {
+
+}
+
+window.ui = ui;
+
+}());
